7f57bdf5e273bcc7763a70f83276b4b763e51444
Frederick <fedmaster217@yahoo.com>
2019-03-21 14:45:12 -0400

    jeez

--- js/sb-admin-3.js
+++ js/sb-admin-3.js
@@ -1,55 +1,10 @@
 (function($) {
   "use strict"; 
     
-    $('#collapsibleA, #collapsibleB, #collapsibleC, #collapsibleD').click(function(){
-        $(this).find('i').toggleClass('fa-caret-square-down fa-caret-square-up')
-    });
-    
-    $("#searchBar").keyup(function () {
-        var value = this.value.toLowerCase().trim();
+var colors = ['red', 'green', 'blue', 'yellow', 'purple', 'hotpink', 'lightgreen', 'darkred', 'lightblue'];
 
-        $("table tr").each(function (index) {
-            if (!index) return;
-            $(this).find("td").each(function () {
-                var id = $(this).text().toLowerCase().trim();
-                var not_found = (id.indexOf(value) == -1);
-                $(this).closest('tr').toggle(!not_found);
-                return not_found;
-            });
-        });
-    });
-    
-    //Adds toggle to visable table for cs-home searchbar
-    $("#searchUsrLanding").keyup(function () {
-        
-        if(!$( "#myDropdown" ).is(":visible"))
-            $( "#myDropdown" ).toggleClass("show");
-        
-        var value = this.value.toLowerCase().trim();
-
-        $("table tr").each(function (index) {
-            if (!index) return;
-            $(this).find("td").each(function () {
-                var id = $(this).text().toLowerCase().trim();
-                var not_found = (id.indexOf(value) == -1);
-                $(this).closest('tr').toggle(!not_found);
-                return not_found;
-            });
-        });
-        
-    });
-    
-    //Adds toggle to visable when window is clicked
-    $(window).click(function(e) {
-        if(!$(e.target).hasClass('')) //td has no class
-            {
-                $( "#myDropdown" ).removeClass("show");
-            }
-    });
-    //setting text of search bar to clicked td element text
-    $("table > tbody > tr").click(function () {
-            $( "#searchUsrLanding" ).val($(this).closest('tr').children('td:first').text());
-            $( "#myDropdown" ).toggleClass("show");
-    });
+$('.profile').each(function() {
+    $(this).css('background-color', colors[Math.floor(Math.random() * colors.length)]);
+});
 
 })(jQuery); // End of use strict
